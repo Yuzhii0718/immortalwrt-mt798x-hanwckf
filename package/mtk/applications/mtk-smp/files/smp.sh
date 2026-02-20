366adc3301a69f730c8fb91fc8d5c3ddb22a76f4
Yuzhii0718 <yuzhii0718@outlook.com>
2026-02-20 11:20:31 +0800

    mt7981: add support for sn-r1

--- package/mtk/applications/mtk-smp/files/smp.sh
+++ package/mtk/applications/mtk-smp/files/smp.sh
@@ -765,6 +765,8 @@ setup_model()
 	newland,nl-wr9103 |\
 	nradio,wt9103 |\
 	ruijie,rg-x30e* |\
+	philips,hy3000 |\
+	sn,r1 |\
 	*7981*)
 		MT7981_whnat $num_of_wifi $usbnet
 		;;
--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -821,3 +821,15 @@ define Device/routerich_ax3000
   DEVICE_PACKAGES := $(MT7981_USB_PKGS)
 endef
 TARGET_DEVICES += routerich_ax3000
+
+define Device/sn_r1
+  DEVICE_VENDOR := SN
+  DEVICE_MODEL := R1
+  DEVICE_DTS := mt7981-sn-r1
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := sn,r1
+  DEVICE_PACKAGES := f2fsck losetup mkf2fs f2fs-tools kmod-fs-f2fs kmod-mmc \
+	luci-app-samba4 e2fsprogs kmod-nvme mmc-utils
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += sn_r1
