92c517dc2b60682e52ea69b9c49d2579dbbbd255
Yuzhii0718 <yuzhii0718@outlook.com>
2026-03-23 10:28:25 +0800

    mt7981: add support for zbtlink_zbt-z81103ax-c-nmbm

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -863,3 +863,21 @@ define Device/zbtlink_zbt-z8103ax-c
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += zbtlink_zbt-z8103ax-c
+
+define Device/zbtlink_zbt-z8103ax-c-nmbm
+  DEVICE_VENDOR := Zbtlink
+  DEVICE_MODEL := ZBT-Z8103AX-C-NMBM
+  DEVICE_DTS := mt7981b-zbtlink-zbt-z8103ax-c-nmbm
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := zbtlink,zbt-z8103ax-c-nmbm
+  DEVICE_PACKAGES := $(MT7981_USB_PKGS) luci-app-samba4
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 117248k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += zbtlink_zbt-z8103ax-c-nmbm
