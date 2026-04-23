f26e90b84bfa75d89ae62f3b02aa82737faf1fb7
Yuzhii0718 <yuzhii0718@outlook.com>
2026-04-23 23:28:06 +0800

    mtwifi-cfg: support definite mac address manually

--- package/mtk/applications/mtwifi-cfg/files/netifd/mtwifi.sh
+++ package/mtk/applications/mtwifi-cfg/files/netifd/mtwifi.sh
@@ -20,10 +20,10 @@ drv_mtwifi_init_device_config() {
 }
 
 drv_mtwifi_init_iface_config() {
-	config_add_string 'ssid:string' macfilter bssid kicklow assocthres steeringthresold
+	config_add_string 'ssid:string' macfilter bssid 'macaddr:macaddr' kicklow assocthres steeringthresold
 	config_add_boolean wmm hidden isolate ieee80211k ieee80211r
 	config_add_int wpa_group_rekey frag rts dtim_period
-	config_add_array 'maclist:list(macaddr)'
+	config_add_array 'maclist:list(macaddr)' 'steeringbssid:list(macaddr)'
 	config_add_boolean mumimo_dl mumimo_ul ofdma_dl ofdma_ul amsdu autoba uapsd
 }
 
