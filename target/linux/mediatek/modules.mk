34b9ebc42fc6d2b38fc1367185b3ec68eb4bbdb0
Yuzhii0718 <yuzhii0718@outlook.com>
2026-04-30 21:37:07 +0800

    mediatek: remove deprecated ppe_cnt parameter

--- target/linux/mediatek/modules.mk
+++ target/linux/mediatek/modules.mk
@@ -2,8 +2,6 @@ define KernelPackage/mediatek_hnat
   SUBMENU:=Network Devices
   TITLE:=Mediatek HNAT module
   DEPENDS:=@TARGET_mediatek +kmod-nf-conntrack
-  AUTOLOAD:=$(call AutoLoad,20,mtkhnat)
-  MODPARAMS.mtkhnat:=ppe_cnt=2
   KCONFIG:= \
 	CONFIG_BRIDGE_NETFILTER=y \
 	CONFIG_NETFILTER_FAMILY_BRIDGE=y \
